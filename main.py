508f2d6009d37fa2b8d7c01e5a35f7b46ce07d25
Carlos Cueto <crcueto25@gmail.com>
2023-01-30 16:23:50 +0200

    Update main script
    Add ctrl+c handler to exit script
    Format the script

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import signal
 import pywinauto
 from HandController import HandController
+
 
 def open_notepad(event):
     event.print_line()
     print(event)
     try:
         app = pywinauto.application.Application().connect(path="notepad.exe")
-        print("notepad is already open")
     except pywinauto.application.ProcessNotFoundError:
         app = pywinauto.application.Application().start("notepad.exe")
         print("notepad is not open")
     app.Notepad.wait("visible")
     return app
 
+
 def save_close_notepad(event):
     event.print_line()
     print(event)
     try:
-        print("notepad is already open")
         app = pywinauto.application.Application().connect(path="notepad.exe")
         app.Notepad.wait("visible")
         app.UntitledNotepad.Edit.type_keys("Hello World!")
@@ -30,13 +31,17 @@ def save_close_notepad(event):
     except pywinauto.application.ProcessNotFoundError:
         print("notepad is not open")
 
+
 config = {
-    'renderer' : {'enable': True},
-    
-    'pose_actions' : [
-        {'name': '1_open_notepad', 'pose':'FIST', 'hand':'right', 'callback': 'open_notepad', "trigger":"enter_leave", "first_trigger_delay":0.3},
-        {'name': '2_close_save_notepad', 'pose':'FIVE', 'hand':'right', 'callback': 'save_close_notepad', "trigger":"enter_leave", "first_trigger_delay":0.3},
+    'renderer': {'enable': True},
+
+    'pose_actions': [
+        {'name': '1_open_notepad', 'pose': 'FIST', 'hand': 'right', 'callback': 'open_notepad', "trigger": "enter_leave", "first_trigger_delay": 0.3},
+        {'name': '2_close_save_notepad', 'pose': 'FIVE', 'hand': 'right', 'callback': 'save_close_notepad', "trigger": "enter_leave", "first_trigger_delay": 0.3},
     ]
 }
 
 HandController(config).loop()
+
+# Manage ctrl+c to stop the program
+signal.signal(signal.SIGINT, signal.SIG_DFL)
